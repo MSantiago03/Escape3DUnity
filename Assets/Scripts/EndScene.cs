e073190779ecb7292968d976cf9bfa225f3a81a6
Manuel Santiago <manuelsantiago096@gmail.com>
2023-12-10 22:16:54 +0100

    [script] added edits to reset lives for new game and key count

--- Assets/Scripts/EndScene.cs
+++ Assets/Scripts/EndScene.cs
@@ -7,6 +7,9 @@ public class EndScene : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        // Resetting vars for new game
+        GameVariables.lives = 4;
+        GameVariables.keyCount = 0;
         SceneManager.LoadScene("EndScene");
     }
 }
